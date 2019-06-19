9dcacb78999d06bdd8f842d11e32656b6d8d6965
F <fredde_lutz@hotmail.com>
2019-06-19 23:32:48 +0200

    design

--- src/App.js
+++ src/App.js
@@ -1,26 +1,124 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <div className='row' style={{textAlign: "center"}}>
+          <h1 className="header">Midsommar på Schlätta 2.0</h1>
+        </div>
+        <div className='row team'>
+          <div className='col-6'>
+            <h2>Lagnamn:</h2>
+          </div>
+          <div className='col-2'>
+            <h3>Lag Ingan</h3>
+          </div>
+          <div className='col-2'>
+            <h3>Lag Ingan</h3>
+          </div>
+          <div className='col-2'>
+            <h3>hejhejhejhejhejhejhejhejeh</h3>
+          </div>
+        </div>
+        <div className='comp fruit'>
+          <div className='row'>
+            <div className='col-6'>
+              <h2>Fruit Ninja</h2>
+            </div>
+            <div className='col-2'>
+              <h3> </h3>
+            </div>
+            <div className='col-2'>
+              <h3> </h3>
+            </div>
+            <div className='col-2'>
+              <h3> </h3>
+            </div>
+          </div>
+        </div>
+        <div className='comp voi'>
+          <div className='row'>
+            <div className='col-6'>
+              <h2>Voi</h2>
+            </div>
+            <div className='col-2'>
+              <h3> </h3>
+            </div>
+            <div className='col-2'>
+              <h3> </h3>
+            </div>
+            <div className='col-2'>
+              <h3> </h3>
+            </div>
+          </div>
+        </div>
+        <div className='comp netflix'>
+          <div className='row'>
+            <div className='col-6'>
+              <h2>Netflix</h2>
+            </div>
+            <div className='col-2'>
+              <h3> </h3>
+            </div>
+            <div className='col-2'>
+              <h3> </h3>
+            </div>
+            <div className='col-2'>
+              <h3> </h3>
+            </div>
+          </div>
+        </div>
+        <div className='comp angry'>
+          <div className='row'>
+            <div className='col-6'>
+              <h2>Angry Birds</h2>
+            </div>
+            <div className='col-2'>
+              <h3> </h3>
+            </div>
+            <div className='col-2'>
+              <h3> </h3>
+            </div>
+            <div className='col-2'>
+              <h3> </h3>
+            </div>
+          </div>
+        </div>
+        <div className='comp slide'>
+          <div className='row'>
+            <div className='col-6'>
+              <h2>Dragon, Fly!</h2>
+            </div>
+            <div className='col-2'>
+              <h3> </h3>
+            </div>
+            <div className='col-2'>
+              <h3> </h3>
+            </div>
+            <div className='col-2'>
+              <h3> </h3>
+            </div>
+          </div>
+        </div>
+        <div className='comp result'>
+          <div className='row'>
+            <div className='col-6'>
+              <h2>Resultat:</h2>
+            </div>
+            <div className='col-2'>
+              <h3> </h3>
+            </div>
+            <div className='col-2'>
+              <h3> </h3>
+            </div>
+            <div className='col-2'>
+              <h3> </h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
