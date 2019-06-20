7d7ba882bd40b79f7d8f45f81c180eff0f675248
unknown <bas15jpe@student.lu.se>
2019-06-20 15:10:57 +0200

    test

--- Server.js
+++ Server.js
@@ -1,19 +1,3 @@
-const { Client } = require('pg');
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
-
-client.connect();
-
-client.query('SELECT table_schema,teams FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
 
 const express = require('express');
 const favicon = require('express-favicon');
